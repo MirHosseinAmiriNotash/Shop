572a194a30b51509553994793fcb0f478123ad23
Hossein <mh.amiri.notash@gmail.com>
2023-07-17 15:09:51 +0430

    main.js added & Slider created with js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let slideindex = 1;
+function setslide(input,index){
+    slideindex = index;
+    let item = document.querySelector(`#${input}`)
+    let slides = [...document.querySelector('.slides').children];
+    slides.forEach((element) => {
+        element.classList.remove('active');
+    })
+
+    item.classList.add('active');
+}
+
+
+setInterval(()=>{
+    slideindex += 1;
+    if(slideindex == 4){
+        slideindex = 1;
+    }
+    setslide(`slide${slideindex}`, slideindex)
+}, 4000)
